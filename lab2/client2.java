3374fb9929b6814801cf6a81e029a1943b5208c1
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-10-09 00:40:26 -0400

    part 2

---
+++ lab2/client2.java
@@ -0,0 +1,78 @@
+import java.net.*;
+import java.io.*;
+
+public class client2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World, I'm client");
+		try{
+
+			Socket socket_control = new Socket("localhost",9879);
+			BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket_control.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			DataOutputStream writer = new DataOutputStream(socket_control.getOutputStream());
+			long time=0;
+			while(true){
+			
+				String str = reader.readLine();
+				writer.writeBytes(str + "\r\n");
+				System.out.println("client: I'd like to get file \""+str+"\"\n");
+
+			    str = socket_reader.readLine();
+				System.out.println("server responese:\""+str+"\"\n");
+
+					if(str.equalsIgnoreCase("File exists, everything works fine! The file transfer is about to begin")){
+						System.out.println("about to accept the file, please type in the name of the file to be stored localy");
+						// read data from socket_control
+						// define once:
+						
+						str = socket_reader.readLine();
+						int port_num=Integer.parseInt(str);
+						Socket socket_data = new Socket("localhost",9899);
+						System.out.println("get new port number "+port_num);
+						DataInputStream socket_dis = new DataInputStream(socket_data.getInputStream());
+
+
+						str = reader.readLine();
+						File file=new File(str);
+						FileOutputStream fos= new FileOutputStream(file);
+						byte[] buffer = new byte[1024];
+						int total=0;
+						long time_start=System.currentTimeMillis();
+						int len;
+						while (true){
+							len =socket_dis.read(buffer);
+							total+=len;
+							if(len<=0){
+								long time_end=System.currentTimeMillis();
+								System.out.println("start time: "+time_start+"  end time: "+time_end);
+								System.out.println("time used: "+(time_end-time_start));
+								System.out.println("done receiving data.");
+								System.out.println("file.length()="+file.length());
+								System.out.println("total character="+total);
+
+								break;
+							}
+							fos.write(buffer, 0, len); //writing a portion of buffer
+						}	
+						fos.close();
+						//socket_control.close();
+						socket_data.close();
+						if(socket_data.isClosed()){
+								System.out.println("data socket is closed and the length of last input stream is "+len);
+						}
+					}
+			
+			
+			//"\r\n" means the end of a packet
+			if(str.equalsIgnoreCase("quit"))
+				break;
+			}
+			socket_control.close();
+		}catch(Exception e){e.printStackTrace();}
+	}
+
+}
--- lab2/server2.java
+++ lab2/server2.java
@@ -1,15 +1,16 @@
 import java.net.*;
 import java.io.*;
-public class server{
+public class server2{
 	public static void main(String[] args){
 		System.out.println("Hello World, I' server");
 		try{	
 			ServerSocket server = new ServerSocket(9879);
-			Socket socket=server.accept();
+			Socket socket_control=server.accept();
+			
 
-			BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket_control.getInputStream()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+			DataOutputStream writer = new DataOutputStream(socket_control.getOutputStream());
 			
 			while(true){
 			String str = socket_reader.readLine();
@@ -25,24 +26,31 @@ public class server{
 				FileInputStream fin= new FileInputStream(file);
 				byte[] buffer = new byte[1024];
 				// define once:
+
+				//new connection send port num to client
+				int port_num=9899;
+				ServerSocket server2 = new ServerSocket(9899);
+				Socket socket_data=server2.accept();
+				writer.writeBytes(port_num+"\r\n");
+
 				DataOutputStream socket_dos = new DataOutputStream(
-						socket.getOutputStream());
+						socket_data.getOutputStream());
 				String CRLF = "\r\n";
-				
+				int len;
 				while(true){
 					//reads up to len bytes of data from input stream to buffer; return the # of bytes
-					int len=fin.read(buffer,0,1024);
-					// write lines to socket:
-					if(len<=0)	{
-						
+					len=fin.read(buffer,0,1024);
+					// write lines to socket_control:
+					if(len<=0)	{	
 						break;
 					}
 					//use everytime:
 					socket_dos.write(buffer, 0, len); //writing a portion of buffer
 				}
 				fin.close();
-				socket.close();
-				break;
+				//socket_control.close();
+				socket_data.close();
+				
 				
 			}
 				
@@ -51,9 +59,9 @@ public class server{
 				writer.writeBytes("The file you claim does not exist"+ "\r\n");
 
 
-			//two way to close the socket:1.file transfer complete 2.client said "quit"
+			//two way to close the socket_control:1.file transfer complete 2.client said "quit"
 			if(str.equalsIgnoreCase("quit")){
-				socket.close();
+				socket_control.close();
 				break;
 			}
 		  }
