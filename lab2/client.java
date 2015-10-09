d621fd6dc4f45a2c6d4fab3f969bfcb577b04474
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-10-08 23:53:47 -0400

    section 1 done

--- lab2/client.java
+++ lab2/client.java
@@ -10,7 +10,7 @@ public class client {
 		System.out.println("Hello World, I'm client");
 		try{
 
-			Socket socket = new Socket("localhost",9878);
+			Socket socket = new Socket("localhost",9879);
 			BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
@@ -28,16 +28,21 @@ public class client {
 						System.out.println("about to accept the file, please type in the name of the file to be stored localy");
 						// read data from socket
 						// define once:
+					
 						DataInputStream socket_dis = new DataInputStream(socket.getInputStream());
 						str = reader.readLine();
 						File file=new File(str);
 						FileOutputStream fos= new FileOutputStream(file);
 						byte[] buffer = new byte[1024];
 						int total=0;
+						long time_start=System.currentTimeMillis();
 						while (true){
 							int len =socket_dis.read(buffer);
 							total+=len;
 							if(len<=0){
+								long time_end=System.currentTimeMillis();
+								System.out.println("start time: "+time_start+"  end time: "+time_end);
+								System.out.println("time used: "+(time_end-time_start));
 								System.out.println("done receiving data.");
 								System.out.println("file.length()="+file.length());
 								System.out.println("total character="+total);
